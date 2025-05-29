928107ba40202a88237ebc412883a2b40d111790
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-05-29 12:05:31 +0530

    ccc

--- basicDjango/chaiDjango/settings.py
+++ basicDjango/chaiDjango/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',  # Custom app for chaiDjango
+    'tailwind'
+    'theme'
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
